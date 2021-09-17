cb53e614e7fb24f119cb7424e25de206388ab95a
sagar dhandhalya <dhandhalya.sagar@searce.com>
2021-09-17 19:50:47 +0530

    buttton component demo test addded

--- src/components/Button/__test__/Button.test.ts
+++
@@ -1,5 +0,0 @@
-test('should button render', () => {
-  expect(2 + 2).toBe(4)
-})
-
-export {}
---
+++ src/components/Button/__test__/Button.test.tsx
@@ -0,0 +1,37 @@
+import Button from '../Button'
+import { fireEvent, render } from '@testing-library/react'
+
+beforeEach(() => {
+  // repetative login go here
+})
+
+test('should Button render with ptops', () => {
+  const { getByTestId } = render(
+    <Button
+      text="Simple Button"
+      onClick={() => {
+        console.log('hi')
+      }}
+      type="button"
+    />
+  )
+
+  const btn = getByTestId('button')
+  expect(btn.textContent).toBe('Simple Button')
+  expect(btn.getAttribute('type')).toBe('button')
+})
+
+test('should onclick on button working', () => {
+  const testFn = jest.fn(() => {
+    console.log('hi')
+  })
+  const { getByTestId } = render(
+    <Button text="Simple Button" onClick={() => testFn()} type="button" />
+  )
+
+  const btn = getByTestId('button')
+  fireEvent.click(btn)
+  expect(testFn.mock.calls.length).toBe(1)
+})
+
+export {}
