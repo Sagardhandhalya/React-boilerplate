959b28591b0b2b115904b0b8c621d07bedd03e25
sagar dhandhalya <dhandhalya.sagar@searce.com>
2021-09-22 18:07:54 +0530

    bulma css framwork set up

---
+++ src/components/organisms/BulmaTest/BulmaTest.tsx
@@ -0,0 +1,38 @@
+const BulmaTest = () => {
+  return (
+    <div className="container mx-6-desktop">
+      <h1 className="is-size-3 my-2 is-text-centered">Bulma test</h1>
+      <div className="columns">
+        <div className="column is-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+          totam ipsam accusantium aperiam? Alias id officia eius porro
+          explicabo. Maiores?
+        </div>
+        <div className="column is-6">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
+          reiciendis laboriosam tempora error sapiente unde omnis qui
+          repudiandae recusandae? Nulla, nemo laudantium molestiae beatae libero
+          quia fuga quam laborum architecto at eos molestias vero doloremque
+          minima perferendis ipsam dolorum suscipit commodi? Ipsam ad tempore
+          molestiae, nemo, omnis vitae doloribus porro est quod officia rem
+          voluptates culpa alias perferendis, dolore harum odit. Doloremque
+          eligendi alias quasi vel id eius placeat esse, tempora asperiores
+          aspernatur libero, perferendis totam magnam ullam nam? Molestias
+          minima sint, at optio alias, cupiditate eius ipsum aut tempora
+          distinctio suscipit natus quia, molestiae id. Aliquam necessitatibus
+          iste quo?
+        </div>
+        <div className="column is-3">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+          excepturi. Quae sapiente voluptas impedit accusantium sunt animi,
+          voluptate iste cum dicta vel inventore corrupti eum maiores nemo, est
+          excepturi similique ducimus facilis nostrum itaque libero saepe enim
+          tenetur. Iste laudantium repudiandae animi alias quas nobis qui
+          debitis repellat accusamus nisi.
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BulmaTest
--- src/pages/FeatureTest/FeatureTest.tsx
+++ src/pages/FeatureTest/FeatureTest.tsx
@@ -1,9 +1,11 @@
+import BulmaTest from '../../components/organisms/BulmaTest/BulmaTest'
 import Todos from '../../components/organisms/Todos/Todos'
 import DataContext from '../../context/DataContext'
 
 const FeatureTest = () => {
   return (
     <div className="container">
+      <BulmaTest />
       <DataContext>
         <Todos />
       </DataContext>
